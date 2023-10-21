a4588fd0eff60f82ef7a064b116b66369b8381e6
nitish <pqnitish2020@gmail.com>
2023-10-21 12:26:33 +0530

    introduction to js

---
+++ problem1.js
@@ -0,0 +1,4 @@
+let name = "Masai";
+let trans = "A Transformation in Education";
+console.log(name);
+console.log(trans);
---
+++ problem2.js
@@ -0,0 +1,6 @@
+let name = "Nitish kumar";
+console.log(name);
+name = "Upendera singh";
+console.log(`father's name:${name}`);
+name = "Indu devi";
+console.log(`mother's name:${name}`);
